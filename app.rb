17b1008ed9e4edc0293b9bdf4b3782892a562bc6
julienXX <julien.blanchard@me.com>
2009-07-13 16:58:49 +0200

    Added twitter_oauth

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'twitter'
+require 'twitter_oauth'
 require 'www/delicious'
 
 # Del.icio.us Auth
@@ -11,11 +11,21 @@ delicious = WWW::Delicious.new('JulienXX', 'Jul*d3lic')
 # twitter = Twitter::Base.new(httpauth)
 
 before do
-  @client = OAuth::Consumer.new(
-    'Ob88dhyY3G6F04NgHZrCA', 
-    'WbWsanzbO8WRcghMBSByIasp7Lyzg63huvYxTGDIw', 
-    {:site => 'http://twitter.com'}
+  # @oauth = OAuth::Consumer.new(
+  #       'Ob88dhyY3G6F04NgHZrCA', 
+  #       'WbWsanzbO8WRcghMBSByIasp7Lyzg63huvYxTGDIw', 
+  #       {:site => 'http://twitter.com'}
+  #     )
+  
+  @user = session[:user]
+  @client = TwitterOAuth::Client.new(
+    :consumer_key => 'Ob88dhyY3G6F04NgHZrCA',
+    :consumer_secret => 'WbWsanzbO8WRcghMBSByIasp7Lyzg63huvYxTGDIw',
+    :token => session[:access_token],
+    :secret => session[:secret_token]
   )
+  @rate_limit_status = @client.rate_limit_status
+  
 end
 
 get '/' do
@@ -25,35 +35,57 @@ get '/' do
   #     erb :index
   # @httpauth = Twitter::HTTPAuth.new('julienXX', 'Julien1980')
   #   @twitter = Twitter::Base.new(@httpauth)
+  redirect '/timeline' if @user
   erb :index
 end
 
-get '/favorites' do
-  erb :show
+get '/timeline' do
+  
+  erb :timeline
 end
 
 # store the request tokens and send to Twitter
 get '/connect' do
-  request_token = @client.get_request_token
+  request_token = @client.request_token
   session[:request_token] = request_token.token
   session[:request_token_secret] = request_token.secret
-  redirect request_token.authorize_url.gsub('authorize', 'authenticate') 
+  redirect request_token.authorize_url
 end
+
+get '/auth' do
+  # Exchange the request token for an access token.
+  @access_token = @client.authorize(
+    session[:request_token],
+    session[:request_token_secret]
+  )
+  
+  if @client.authorized?
+      # Storing the access tokens so we don't have to go back to Twitter again
+      # in this session.  In a larger app you would probably persist these details somewhere.
+      session[:access_token] = @access_token.token
+      session[:secret_token] = @access_token.secret
+      session[:user] = true
+      redirect '/timeline'
+    else
+      redirect '/'
+  end
+end
+
+
 
 use_in_file_templates!
 
 __END__
 
-@@index
+@@ index
 <div id="connect_button">
     <a href="/connect"><img src="sign_in_with_twitter.gif"/></a>
 </div>
 
-@@ show
+@@ timeline
 <h1>Favorites</h1>
 <ul>
-  <% @twitter.favorites(:page => 2).each do |tweet| %>
-  <li><%= tweet.text %></li>
-  <% end %>
+  <%= @client.public_timeline.each{ |tweet| puts tweet.inspect } %>
+  
 </ul>
 
