e096e9f7d0978e42f04eb6653e636982acf8ac41
julienXX <julien.blanchard@me.com>
2009-07-17 17:53:46 +0200

    changes checkboxes name

--- app.rb
+++ app.rb
@@ -86,8 +86,8 @@ end
 
 post '/bookmark' do
   @client.favorites.each do |tweet|
-    @check = 'check_' + tweet['id'].to_s
-    if params[:@check] != nil
+    #check_id = 'check_' + tweet['id'].to_s
+    if #{params[:check + tweet['id'].to_s]} != nil
       link_regex = /(http:\S+)/    
       links = tweet['text'].scan(link_regex)[0]
       content = tweet['text'].gsub(link_regex, '')
