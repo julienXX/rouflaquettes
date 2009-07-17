de39eafdcbe95448446a58abc9a33ae7bbcfd22e
julienXX <julien.blanchard@me.com>
2009-07-17 18:56:31 +0200

    implemented bookmark

--- app.rb
+++ app.rb
@@ -86,7 +86,7 @@ end
 
 post '/bookmark' do
   @client.favorites.each do |tweet|
-    if params["check_#{tweet['id'].to_s}"] != nil
+    if params["check_#{tweet['id']}"] != nil
       link_regex = /(http:\S+)/    
       links = tweet['text'].scan(link_regex)[0]
       content = tweet['text'].gsub(link_regex, '')
