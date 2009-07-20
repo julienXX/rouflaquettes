50142fcc39e7ff4c06ee4071a0cec58ad317553c
julienXX <julien.blanchard@me.com>
2009-07-20 12:47:30 +0200

    test

--- app.rb
+++ app.rb
@@ -93,7 +93,7 @@ post '/bookmark' do
       content = tweet['text'].gsub(link_regex, '')
     
       #delicious.posts_add(:url => links[0], :title => content, :notes => 'Imported from Twitter')
-      #{links[0]}
+      #{tweet['id']}
     end
   end
 end
