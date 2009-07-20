bf0757574346bbd17d43993a84a34af5685c9ee3
julienXX <julien.blanchard@me.com>
2009-07-20 12:45:38 +0200

    test

--- app.rb
+++ app.rb
@@ -93,7 +93,7 @@ post '/bookmark' do
       content = tweet['text'].gsub(link_regex, '')
     
       #delicious.posts_add(:url => links[0], :title => content, :notes => 'Imported from Twitter')
-      haml '<%= tweet['id'] %>'
+      erb: <% links[0] %>
     end
   end
 end
