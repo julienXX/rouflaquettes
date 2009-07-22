41dc410cb4af628c3058472418f43d17bef58cab
julienXX <julien.blanchard@me.com>
2009-07-22 15:05:43 +0200

    tests

--- app.rb
+++ app.rb
@@ -89,7 +89,7 @@ post '/bookmark' do
   @ids = Array.new
   @i = 1
   @client.favorites.each do |tweet|
-    if (params["check_#{tweet['id']}"]).nil?
+    if params["check_#{tweet['id']}"] != nil
       @ids.push(@i.to_s + ',')
       @i = @i +1
     else
