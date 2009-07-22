84b919bc79957b1c0f6eae8d22fff483100c55cb
julienXX <julien.blanchard@me.com>
2009-07-22 15:20:45 +0200

    tests

--- app.rb
+++ app.rb
@@ -89,13 +89,13 @@ post '/bookmark' do
   @ids = Array.new
   @client.favorites.each do |tweet|
     @param = params["check_#{tweet['id']}"]
-    if params["check_#{tweet['id']}"].nil?
-      @ids.push(@param)
-    else
-      @ids.push(@param)
-    end
+    # if params["check_#{tweet['id']}"].nil?
+    #       @ids.push(@param)
+    #     else
+    #       @ids.push(@param)
+    #     end
   end
-  erb "id selected: <%= @ids %>"
+  erb "id selected: <%= @param %>"
 
   # link_regex = /(http:\S+)/    
   #   links = tweet['text'].scan(link_regex)[0]
