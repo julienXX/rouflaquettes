f06c57496e6afb9e6c95a7b635a5e3026535c5e8
julienXX <julien.blanchard@me.com>
2009-07-22 15:47:24 +0200

    tests

--- app.rb
+++ app.rb
@@ -87,7 +87,7 @@ end
 
 post '/bookmark' do
   @client.favorites.each do |tweet|
-    if params["#{tweet['id']}"] != nil
+    if params['#{tweet['id']}'] != nil
       link_regex = /(http:\S+)/    
       links = tweet['text'].scan(link_regex)[0]
       content = tweet['text'].gsub(link_regex, '')
