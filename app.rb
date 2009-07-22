532a98f582d7056dd781aaf8d78559b2e4878fd1
julienXX <julien.blanchard@me.com>
2009-07-22 14:20:43 +0200

    tests

--- app.rb
+++ app.rb
@@ -92,7 +92,7 @@ post '/bookmark' do
     if params["check_#{tweet['id']}"] == 0
       @ids.push('toto')
     else
-      @ids.push("#{tweet['id']}")
+      @ids.push("#{tweet['id']},")
     end
   end
   erb "id selected: <%= @ids %>"
