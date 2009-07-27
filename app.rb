3f4c30ecbdce6080d002c1c0c7d29e2babeead74
julienXX <julien.blanchard@me.com>
2009-07-27 12:00:29 +0200

    keep selected tweets between pages

--- app.rb
+++ app.rb
@@ -30,6 +30,7 @@ before do
   end
   content_type CONTENT_TYPES[request_uri], :charset => 'utf-8'
   
+  @statuses = Array.new
   @page = 1
 end
 
@@ -40,10 +41,19 @@ end
 
 get '/timeline' do
   @tweets = @client.favorites
+  
+  params[:tweets].each do |tweet|
+    @statuses.push(tweet)
+  end if params[:tweets]
+  
   erb :timeline
 end
 
 get '/timeline/:page' do
+  params[:tweets].each do |tweet|
+    @statuses.push(tweet)
+  end if params[:tweets]
+  
   @page = params[:page]
   @tweets = @client.favorites(params[:page])
   erb :timeline
@@ -86,7 +96,7 @@ get '/disconnect' do
 end
 
 post '/bookmark' do
-  @statuses = Array.new
+  
   params[:tweets].each do |tweet|
     @statuses.push(tweet)
   end if params[:tweets]
