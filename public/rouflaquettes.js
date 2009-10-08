a215992f308089fbab7849967ee8f9631178c72d
julienXX <julien.blanchard@me.com>
2009-10-08 16:39:55 +0200

    ajaxify stuff

--- public/rouflaquettes.js
+++ public/rouflaquettes.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   
   $('#del').hide();
-  $('input[id='bookmark']').click(function() {
+  $('input[id=bookmark]').click(function() {
            $('#del').show();
          });
      });
