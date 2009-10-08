b25c67429ab87275dcc21b2b2a6c36661412ff36
julienXX <julien.blanchard@me.com>
2009-10-08 15:16:35 +0200

    ajaxify stuff

--- public/rouflaquettes.js
+++ public/rouflaquettes.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $('div[id=delicious_credentials]').hide();
-  $('input[type=submit]').bind('submit',function() {
-     $('div[id=delicious_credentials]').show();
+  $('div.delicious_credentials').hide();
+  $('input[type=submit]').click('submit',function() {
+     $('div.delicious_credentials').show();
    });
  });
