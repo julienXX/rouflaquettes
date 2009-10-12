3f39258e9110267c60fb2b36a706a2d3de0a07b3
julienXX <julien.blanchard@me.com>
2009-10-12 12:15:23 +0200

    trying jquery.pageslide

--- public/rouflaquettes.js
+++ public/rouflaquettes.js
@@ -1,9 +1,9 @@
-// $(document).ready(function(){
-// 
-//   $('#del').hide();
-// 
-//   $('a#del_show').click(function(){
-//     $('#del').show('slow');
-//   });
-// 
-// });
+$(document).ready(function(){
+
+  $('#del').hide();
+
+  $('a#del_show').click(function(){
+    $('#del').show('slow');
+  });
+
+});
